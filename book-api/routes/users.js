093f9839ae4e5a921a48176d25b49212e6625b79
Khanh <khanhpham2001pm@gmail.com>
2025-10-06 14:59:01 +0200

    deleted duplicated file

--- book-api/routes/users.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const usersController = require('../controllers/usersController');
-
-router.get('/', usersController.getAllUsers);
-router.get('/:id', usersController.getUserById);
-
-module.exports = router;
