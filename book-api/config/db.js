b0bde56f5ff12e9c1e7433955c88ee14aa61c714
Khanh <khanhpham2001pm@gmail.com>
2025-10-11 14:18:50 +0200

    Change from createConnection to createPool

--- book-api/config/db.js
+++ book-api/config/db.js
@@ -1,16 +1,16 @@
 require('dotenv').config();
-const mysql = require('mysql2');
-const db = mysql.createConnection({
+const mysql = require('mysql2/promise');
+
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
-db.connect((err) => {
-  if (err) {
-    console.error('Database connection failed:', err.stack);
-    return;
-  }
-  console.log('Connected to MySQL');
-});
-module.exports = db;
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 10,
+  idleTimeout: 60000,
+  queueLimit: 0
+})
+
+module.exports = pool;
