7a2badeeae596d7365e321173e59a85a355c6a02
Khanh <118523784+sheloveskhanh@users.noreply.github.com>
2025-10-11 21:57:53 +0200

    Async await refactor (#2)
    * Change from createConnection to createPool
    
    * Async await refactor

--- book-api/controllers/usersController.js
+++ book-api/controllers/usersController.js
@@ -1,77 +1,72 @@
-const db = require('../config/db');
+const pool = require('../config/db');
 const bcrypt = require('bcrypt');
 
-exports.getAllUsers = (req, res) => {
-  db.query('SELECT user_id, email, display_name, role, created_at, updated_at FROM users', (err, results) => {
-    if (err) {
-      return res.status(500).json({ message: err.message });
-    }
-    res.json(results);
-  });
+exports.getAllUsers = async (req, res) => {
+  try {
+    const [results] = await pool.query('SELECT user_id, email, display_name, role, created_at, updated_at FROM users');
+    return res.json(results)
+  } catch (err) {
+    return res.status(500).json({ message: err.message })
+  }
 };
 
-exports.getUserById = (req, res) => {
-  const userId = req.params.id;
-  db.query('SELECT user_id, email, display_name, role, created_at, updated_at FROM users WHERE user_id = ?', [userId], (err, results) => {
-    if (err) {
-      return res.status(500).json({ message: err.message });
-    }
+exports.getUserById = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const [results] = await pool.query('SELECT user_id, email, display_name, role, created_at, updated_at FROM users WHERE user_id = ?', [userId]);
     if (results.length === 0) {
-      return res.status(404).json({ message: 'User not found' });
+      return res.status(404).json({ message: `User ${userId} not found` })
     }
-    res.json(results[0]);
-  });
+    return res.json(results)
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
 };
 
-exports.createUser = (req, res) => {
-  const { email, display_name, password } = req.body;
+exports.createUser = async (req, res) => {
+  try {
+    const { email, display_name, password } = req.body;
     if (!email || !display_name || !password) {
-    return res.status(400).json({ message: 'Email, display name, and password are required' });
-  }
-  if (password.length < 6) {
-    return res.status(400).json({ message: 'Password must be at least 6 characters long' });
-  }
-  bcrypt.hash(password, 10, (err, password_hash) => {
-    if (err) {
-      return res.status(500).json({ message: 'Error hashing password' });
+      return res.status(400).json({ message: 'Email, display name, and password are required' });
     }
-    const sql = 'INSERT INTO users (email, display_name, password_hash) VALUES (?, ?, ?)';
-    db.query(sql, [email, display_name, password_hash], (err, result) => {
-      if (err) {
-        if (err.code === 'ER_DUP_ENTRY') {
-          return res.status(409).json({ message: 'Email already exists' });
-        }
-        return res.status(500).json({ message: err.message });
-      }
-      res.status(201).json({ message: 'User created' });
-    });
-  });
+    if (password.length < 6) {
+      return res.status(400).json({ message: 'Password must be at least 6 characters long' });
+    }
+    const password_hash = await bcrypt.hash(password, 10);
+    const [results] = await pool.query('INSERT INTO users (email, display_name, password_hash) VALUES (?, ?, ?)', [email, display_name, password_hash]);
+    return res.status(201).json({ message: 'User created', userId: results.insertId });
+  } catch (err) {
+    if (err.code === 'ER_DUP_ENTRY') {
+      return res.status(409).json({ message: 'Email already exists' });
+    }
+    return res.status(500).json({ message: err.message });
+  }
 };
 
-exports.updateUser = (req, res) => {
-  const userId = req.params.id;
-  const { display_name } = req.body;
-  const sql = 'UPDATE users SET display_name=? WHERE user_id=?';
-  db.query(sql, [display_name, userId], (err, result) => {
-    if (err) {
-      return res.status(500).json({ message: err.message });
-    }
-    if (result.affectedRows === 0) {
+exports.updateUser = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const { display_name } = req.body;
+    const [results] = await pool.query('UPDATE users SET display_name=? WHERE user_id=?', [display_name, userId]);
+    if (results.affectedRows === 0) {
       return res.status(404).json({ message: 'User not found' });
     }
-    res.json({ message: 'User updated' });
-  });
+    return res.json(results);
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
 };
 
-exports.deleteUser = (req, res) => {
-  const userId = req.params.id;
-  db.query('DELETE FROM users WHERE user_id=?', [userId], (err, result) => {
-    if (err) {
-      return res.status(500).json({ message: err.message });
-    }
-    if (result.affectedRows === 0) {
+exports.deleteUser = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const [results] = await pool.query('DELETE FROM users WHERE user_id=?', [userId]);
+    if (results.affectedRows === 0) {
       return res.status(404).json({ message: 'User not found' });
     }
-    res.json({ message: 'User deleted' });
-  });
+    return res.json(results)
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
 };
+
